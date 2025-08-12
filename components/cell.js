61233e6c66a546320871c8bea334b8b140a3d3cd
fletchgraham <sfletchgraham@gmail.com>
2025-08-12 14:06:37 -0600

    move style to vars

--- components/cell.js
+++ components/cell.js
@@ -1,36 +1,34 @@
 "use client";
 
 export default function Cell({ value, onChange }) {
+  const containerStyle = {
+    display: "flex",
+    width: "100%",
+    height: "100%",
+    minWidth: 0,
+    minHeight: 0,
+  };
+
+  const inputStyle = {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    minWidth: 0,
+    minHeight: 0,
+    boxSizing: "border-box",
+    border: ".5px solid #000",
+    background: "blue",
+    fontFamily: "monospace",
+    fontSize: "0.85rem",
+    textAlign: "center",
+    padding: 0,
+    margin: 0,
+    outline: "none",
+  };
+
   return (
-    <div
-      style={{
-        display: "flex",
-        width: "100%",
-        height: "100%",
-        minWidth: 0,
-        minHeight: 0,
-      }}
-    >
-      <input
-        value={value}
-        onChange={onChange}
-        style={{
-          flex: 1,
-          width: "100%",
-          height: "100%",
-          minWidth: 0,
-          minHeight: 0,
-          boxSizing: "border-box",
-          border: ".5px solid #000",
-          background: "blue",
-          fontFamily: "monospace",
-          fontSize: "0.85rem",
-          textAlign: "center",
-          padding: 0,
-          margin: 0,
-          outline: "none",
-        }}
-      />
+    <div style={containerStyle}>
+      <input value={value} onChange={onChange} style={inputStyle} />
     </div>
   );
 }
